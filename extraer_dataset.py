ff8a34893babbbf2ef0b671282bf0450d077a0ae
martinhomilla <martin02alonso@gmail.com>
2025-06-25 11:55:34 +0200

    update plots and logs, represent dataset with UMAP and tries of make_classification

--- extraer_dataset.py
+++
@@ -1,5 +0,0 @@
-from sklearn.datasets import fetch_california_housing
-
-data = fetch_california_housing(as_frame=True).frame
-data.to_csv('california_housing.csv', index=False)
-import pandas as pd
